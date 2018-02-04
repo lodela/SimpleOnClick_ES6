1e0edfe583157928faa90b2dae6dca7e2b0ffb73
Norberto <lodela.n@gmail.com>
2018-02-04 17:38:40 -0600

    made methods private...

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import {LikesComponent} from './likes.component';
 
 let component = new LikesComponent(10,true);
+
 component.onClick();
 
 console.log(`LikesCount: ${component.likesCount}, isSelected: ${component.isSelected}`);
